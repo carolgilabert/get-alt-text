69f6e9a2e6f96a52bfdb01270c4eeee2a59535bd
Carolina Gilabert <carolgilabert@gmail.com>
2021-05-15 16:36:14 +0100

    Adding function code.

--- functions/get-alt-text.js
+++ functions/get-alt-text.js
@@ -1,6 +1,38 @@
-exports.handler = (event, context) => {
-    return {
-        statusCode: 200,
-        body: 'Hey hey!'
-    };
+const fetch = require('node-fetch');
+const { BEARER_TOKEN } = process.env;
+
+exports.handler = async (event, context) => {
+    try{
+        const tweetId = event.queryStringParameters.id;
+        const rawResponse = await fetch(`https://api.twitter.com/1.1/statuses/lookup.json?id=${tweetId}&include_ext_alt_text=true&trim_user=true&tweet_mode=extended`, {
+            headers: {
+                'Authorization': `Bearer ${BEARER_TOKEN}`
+            }
+        });
+        const tweetData = await rawResponse.json();
+        const images = tweetData[0]?.extended_entities?.media;
+        
+        if (images) {
+            const alts = [];
+            images.forEach(image => {
+                if ('ext_alt_text' in image) {
+                    alts.push(image.ext_alt_text);
+                }
+            });
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify(alts)
+            };
+        }
+        
+        return {
+            statusCode: 204
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: `Whoops! Something went wrong -> ${err}`
+        }
+    }
 };
